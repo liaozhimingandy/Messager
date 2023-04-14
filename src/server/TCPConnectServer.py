6cb8c7e8c7ae6ca5f9793c2db40c98ab4f0075d3
liaozhiming <liaozhimingandy@qq.com>
2023-04-14 16:07:05 +0800

    1.优化文件结构;

--- src/server/TCPConnectServer.py
+++ src/server/TCPConnectServer.py
@@ -17,26 +17,28 @@ class MessageProtocol(protocol.Protocol, policies.TimeoutMixin):
         self.connected = 1
         self.factory.numConnections += 1
 
-    def connectionMade(self):  # 建立连接后的回调函数
+    def connectionMade(self):
+        """建立连接后的回调函数"""
+
         # 判断最大连接数,如果超过则断开连接
-        if(self.factory.numConnections >= self.factory.MAX_CONNECTIONS): 
+        if self.factory.numConnections >= self.factory.MAX_CONNECTIONS:
             self.connected = 0
-            self.transport.write('Too many connections, try again later')
+            self.transport.write("Too many connections, try again later".encode("utf-8"))
             self.transport.loseConnection()
             return
 
-        print(f"ip:{self.transport.getPeer().host},port:{self.transport.getPeer().port}......已建立连接;目前共{self.factory.numConnections}个连接...")
+        print(f"ip:{self.transport.getPeer().host},port:{self.transport.getPeer().port}......已建立连接;"
+              f"目前共{self.factory.numConnections}个连接...")
         # self.transport.write("hello".encode("utf8"))
         ProtobufData(self.transport)
-        
 
-    def dataReceived(self, data): 
-        '''
+    def dataReceived(self, data):
+        """
         接收到客户端的数据
         :param data:
         :return:
-        '''
-        self.resetTimeout() # 重置超时
+        """
+        self.resetTimeout()  # 重置超时
         self.transport.write("ok".encode("utf-8"))
         # 解包
         # 根据不同的指令,完成不同的任务
@@ -45,14 +47,16 @@ class MessageProtocol(protocol.Protocol, policies.TimeoutMixin):
     def connectionLost(self, reason=connectionDone):  # 在连接关闭时调用
         self.factory.numConnections -= 1
         self.setTimeout(None)
-        if(self.connected):
-            print(f"ip:{self.transport.getPeer().host},port:{self.transport.getPeer().port}......已断开连接;目前共{self.factory.numConnections}个连接...")
+        if self.connected:
+            print(f"ip:{self.transport.getPeer().host},port:{self.transport.getPeer().port}......已断开连接;"
+                  f"目前共{self.factory.numConnections}个连接...")
 
 
 class MessageFactory(Factory):
     numConnections = 0
     MAX_CONNECTIONS = 2
-    def __init__(self, quote=None, timeout=120): 
+
+    def __init__(self, quote=None, timeout=120):
         self.timeout = timeout
 
     def buildProtocol(self, addr):
@@ -81,18 +85,19 @@ def ProtobufData(transport):
 
 
 if __name__ == '__main__':
-    SERVER_PORT = 52053 # 监听的端口
-    TIMEOUT = 120 # 超时时间
+    SERVER_PORT = 52053  # 监听的端口
+    TIMEOUT = 120  # 超时时间
     host = "localhost"
     from optparse import OptionParser
-    #设置server启动选项
+
+    # 设置server启动选项
     parser = OptionParser()
     parser.add_option("--host", default=host,
-        dest="host", help="host address [default: %default]")
+                      dest="host", help="host address [default: %default]")
     parser.add_option("-p", "--port", default=SERVER_PORT,
-        dest="port", help="Application port number [default: %default]")
+                      dest="port", help="Application port number [default: %default]")
     (opt, args) = parser.parse_args()
     print(f"Running Socket AMF gateway on {opt.port}")
     reactor.listenTCP(int(opt.port), MessageFactory(timeout=TIMEOUT))
-    print(f"{'*'*8}正在监听本地端口:{int(opt.port)}{'*'*8}")
+    print(f"{'*' * 8}正在监听本地端口:{int(opt.port)}{'*' * 8}")
     reactor.run()
